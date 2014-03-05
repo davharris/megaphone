157bd506802a3b839cc715fe9d5af918294ef343
davharris <harry491@gmail.com>
2014-03-04 21:43:16 -0800

    update example

--- inst/example.R
+++ inst/example.R
@@ -1,4 +1,11 @@
 load("../mistnet-manuscript/x.Rdata")
 library(e1071)
-object = megaphone.fit(x)
+object = megaphone.fit(scale(x), gamma = 1/8, nu = .05)
+
+object
+
 object = add.samples.brute(object)
+
+estimate.volume(object)
+
+plot(object)
