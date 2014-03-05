a56c3bfe3ddcfca1aa5ac2f357474e887201ab02
davharris <harry491@gmail.com>
2014-03-04 21:55:39 -0800

    upload x.Rdata; update example

--- inst/example.R
+++ inst/example.R
@@ -1,11 +1,14 @@
-load("../mistnet-manuscript/x.Rdata")
-library(e1071)
-object = megaphone.fit(scale(x), gamma = 1/8, nu = .05)
+load("data/x.Rdata")
+
+object = megaphone.fit(scale(x), gamma = 1/2, nu = .05)
 
 object
 
-object = add.samples.brute(object)
+for(i in 1:5){
+  message("adding samples: round ", i)
+  object = add.samples.brute(object)
+}
 
 estimate.volume(object)
 
-plot(object)
+plot(object, col = "black")
